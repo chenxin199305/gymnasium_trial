a06c4188d6e394c875d9b17b3878e08b7306682d
JasonChen <chenxin199305@163.com>
2025-05-17 23:28:52 +0800

    refine content

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gymnasium_trial
+
 gymnasium_trial
+
+参考文档：
+
+- https://gymnasium.org.cn
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gymnasium_trial',
+    version='0.0.1',
+    author='Jason Chen',
+    packages=find_packages(),
+    install_requires=[
+        "gymnasium",
+    ],
+)
